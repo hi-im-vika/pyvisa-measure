b0d5ebaef7f8daf93378e879c0f651a347b80262
vika <37137412+hi-im-vika@users.noreply.github.com>
2026-04-15 16:18:24 -0700

    Add demo code from IoT Expert

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 import pyvisa
+import time
 
 def main():
     print("Hello from pyvisa-measure!")
+
+    # check if pyvisa is working
     rm = pyvisa.ResourceManager()
-    print(rm.list_resources())
+    resources = rm.list_resources()
+    print(resources)
+
+    # demo code from https://iotexpert.com/pyvisa-first-use/
+    vm = rm.open_resource(resources[0])
+
+    # Turn on remote mode (so that SCPI commands work)
+    vm.write('SYST:REM')
+    vm.write('APPL CH1, 12V, 1A')
+    vm.write('OUTP ON')
+    print('Power on... waiting for settling')
+    # need a little delay right here
+    time.sleep(3)
+    print('Taking measurements')
+    voltCH1 = float(vm.query('MEAS:VOLT? CH1'))
+    currentCH1 = float(vm.query('MEAS:CURR? CH1'))
+    powerCH1 = voltCH1 * currentCH1
+    print(f'V={voltCH1:.1f}V I={currentCH1:.2f}A P={powerCH1:.2f}W')
+    vm.write('OUTP OFF')
+    print('Turned off power')
 
 if __name__ == "__main__":
     main()
