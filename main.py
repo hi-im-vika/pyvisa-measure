273fa0ffe367069e7095c59aab99fb85e95f8f2e
vika <37137412+hi-im-vika@users.noreply.github.com>
2026-04-15 16:37:41 -0700

    Explain voltage and current value selection

--- main.py
+++ main.py
@@ -14,16 +14,21 @@ def main():
 
     # Turn on remote mode (so that SCPI commands work)
     vm.write('SYST:REM')
-    vm.write('APPL CH1, 12V, 1A')
+    vm.write('APPL CH1, 3.7V, 4A')  # simulate 18650 battery
     vm.write('OUTP ON')
     print('Power on... waiting for settling')
     # need a little delay right here
     time.sleep(3)
     print('Taking measurements')
-    voltCH1 = float(vm.query('MEAS:VOLT? CH1'))
-    currentCH1 = float(vm.query('MEAS:CURR? CH1'))
-    powerCH1 = voltCH1 * currentCH1
-    print(f'V={voltCH1:.1f}V I={currentCH1:.2f}A P={powerCH1:.2f}W')
+    try:
+        while True:
+            voltCH1 = float(vm.query('MEAS:VOLT? CH1'))
+            currentCH1 = float(vm.query('MEAS:CURR? CH1'))
+            powerCH1 = voltCH1 * currentCH1
+            print(f'V={voltCH1:.1f}V I={currentCH1:.2f}A P={powerCH1:.2f}W')
+            # time.sleep(1)
+    except:
+        pass
     vm.write('OUTP OFF')
     print('Turned off power')
 
